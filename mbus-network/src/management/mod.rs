8e0da17925ad1b8d6418855f9abbe61053a0384c
Raghava-Ch <raghava.ch89@gmail.com>
2026-05-22 11:11:01 +0530

    tranport adapter crates feature gates updated

--- mbus-network/src/management/mod.rs
+++ mbus-network/src/management/mod.rs
@@ -1,13 +1,13 @@
-#[cfg(not(target_arch = "wasm32"))]
+#[cfg(all(feature = "tcp-client", not(target_arch = "wasm32")))]
 pub mod std_transport;
 
-#[cfg(not(target_arch = "wasm32"))]
+#[cfg(all(feature = "tcp-server", not(target_arch = "wasm32")))]
 pub mod server_transport;
 
-#[cfg(all(feature = "async", not(target_arch = "wasm32")))]
+#[cfg(all(feature = "tcp-async", not(target_arch = "wasm32")))]
 pub mod async_transport;
 
-#[cfg(all(feature = "wasm", target_arch = "wasm32"))]
+#[cfg(all(feature = "ws-client", target_arch = "wasm32"))]
 pub mod wasm_transport;
 
 #[cfg(all(feature = "ws-server", not(target_arch = "wasm32")))]
